f1eb0514913ef2532fa6d42e68c6b883ae99e8e3
Brian Muñoz <brianmunozjop@gmail.com>
2023-02-12 17:17:15 -0300

    add ideas in a user profile view

--- ideasapp frontend/src/views/userProfile/userProfile.jsx
+++ ideasapp frontend/src/views/userProfile/userProfile.jsx
@@ -2,8 +2,8 @@ import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
 import { useIdea, IdeaProvider } from '../../context/idea.context';
-import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import IdeaPreview from '../../components/idea/IdeaPreview';
 export default () =>
   <IdeaProvider>
     <UserProfile></UserProfile>
@@ -22,8 +22,8 @@ const UserProfile = () => {
   useEffect(() => {
     getIdeas();
   }, []);
-  let ideasfiltradas = ideas.filter(useridea => useridea.author._id === userID).length;
-  data.totalIdeas = ideasfiltradas;
+  let ideasfiltradas = ideas.filter(useridea => useridea.author._id === userID);
+  data.totalIdeas = ideasfiltradas.length;
 
 
 
@@ -45,17 +45,20 @@ const UserProfile = () => {
 
 
   return (
-    <Card style={{ maxWidth: '240px', minWidth: '200px', margin: '0 auto' }} className="mt-5">
+    <Card style={{ maxWidth: '320px', minWidth: '200px', margin: '0 auto' }} className="mt-5">
       {data.img_profile ? <Card.Img variant="top" src={data.img_profile} alt={data.name} />
         : <Card.Img variant="top" src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_640.png" alt={data.name}/>
       }
       <Card.Body>
         <Card.Title className="text">@{data.username}</Card.Title>
-        <Card.Text className="font-weight-bold" >
+        <Card.Text  >
           Name: <span className="text-dark" >{data.name}</span>
         </Card.Text>
+        <Card.Text className="fw-bold" >
+          Ideas <span className="text-dark" >{'('+data.totalIdeas+")"}</span>
+        </Card.Text>
+        {data.totalIdeas > 0&& ideasfiltradas.map((el) => { return <IdeaPreview id={el._id} idea={el.idea} description={el.description} maxChar="50"></IdeaPreview> })}
 
-        <Button variant="primary">Total Ideas: {data.totalIdeas}</Button>
       </Card.Body>
     </Card>
   );
